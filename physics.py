dbfd6250c847077edbe28e9cc5ac777d408e31d7
Mikko Rautiainen <m_rautiainen@hotmail.com>
2013-10-26 17:06:53 +0300

    refactor physics calculations

--- physics.py
+++ physics.py
@@ -18,11 +18,12 @@ class PhysicsManager():
     def update(self, dt):
         # print time.time() - self.prev_time
         self.seq += 1
+        print self.balls[0].vel
         for ball in self.balls:
-            mx = self.input_state.get(ball.ident, 'x')
-            my = self.input_state.get(ball.ident, 'y')
-            self._update_acc(ball, mx, my)
-            self._update_vel(ball, mx, my, dt)
+            movement = eu.Vector2(self.input_state.get(ball.ident, 'x'), self.input_state.get(ball.ident, 'y'))
+            movement.normalize()
+            self._update_acc(ball, movement)
+            self._update_vel(ball, dt)
         self.collide()
         self.collide_walls()
         for ball in self.balls:
@@ -30,59 +31,21 @@ class PhysicsManager():
             # print ball.pos, dt, self.seq
         self.prev_time = time.time()
 
-    def _update_acc(self, ball, mx, my):
+    def _update_acc(self, ball, movement):
         """Update acceleration"""
-        if mx:
-            if abs(ball.vel.x) < ball.max_vel or copysign(1, ball.vel.x) != mx:
-                # ball is not at max speed or acc has opposite direction
-                ball.acc.x = mx * ball.accValue
-            else:
-                ball.acc.x = 0 # max speed achieved, continue with it
-        elif ball.vel.x:
-            # if key not pressed, decelerate, i.e. accelerate with opposite sign from velocity
-            ball.acc.x = -copysign(ball.decel, ball.vel.x)
+        ball.acc = eu.Vector2(0, 0)
+        if movement.magnitude() > 0:
+            #acceleration from player's controls
+            ball.acc += movement * ball.accValue
+            #deceleration to make sure player can't accelerate past max_vel
+            ball.acc -= ball.vel * (ball.accValue/ball.max_vel)
+        else:
+            #constant deceleration from floor
+            ball.acc -= ball.vel.normalized()*ball.decel
 
-        if my:
-            if abs(ball.vel.y) < ball.max_vel or copysign(1, ball.vel.y) != my:
-                ball.acc.y = my * ball.accValue
-            else:
-                ball.acc.y = 0
-        elif ball.vel.y:
-            ball.acc.y = -copysign(ball.decel, ball.vel.y)
-
-    def _update_vel(self, ball, mx, my, dt):
+    def _update_vel(self, ball, dt):
         """Update velocity"""
-        if ball.acc.x:
-            dv = ball.acc.x * dt
-            if copysign(1, ball.acc.x) == copysign(1, ball.vel.x):
-                # increasing velocity
-                ball.vel.x += dv
-                if abs(ball.vel.x) > ball.max_vel:
-                    # set to max_vel, same direction
-                    ball.vel.x = copysign(ball.max_vel, ball.vel.x)
-            else:
-                # decelerating
-                if abs(ball.vel.x) < abs(dv) and not mx:
-                    # velocity change larger than velocity itself and we are not accelerating
-                    ball.vel.x = 0
-                else:
-                    ball.vel.x += dv
-
-        if ball.acc.y:
-            dv = ball.acc.y * dt
-            if copysign(1, ball.acc.y) == copysign(1, ball.vel.y):
-                # increasing velocity
-                ball.vel.y += ball.acc.y * dt
-                if abs(ball.vel.y) > ball.max_vel:
-                    # set to max_vel, same direction
-                    ball.vel.y = copysign(ball.max_vel, ball.vel.y)
-            else:
-                # decelerating
-                if abs(ball.vel.y) < abs(dv) and not my:
-                    # velocity change larger than velocity itself and we are not accelerating
-                    ball.vel.y = 0
-                else:
-                    ball.vel.y += dv
+        ball.vel += ball.acc * dt
 
     def update_pos(self, ball, dt):
         pos = ball.pos
