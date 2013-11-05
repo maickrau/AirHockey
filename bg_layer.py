25d1f6457f0c865503e16e11bd381af4d1ba6c1a
Niklas <nyholmniklas@gmail.com>
2013-11-05 14:37:34 +0200

    Background now draws properly in the center

--- bg_layer.py
+++ bg_layer.py
@@ -5,5 +5,5 @@ class BgLayer(cocos.layer.Layer):
 
     def __init__(self):
         super(BgLayer, self).__init__()
-        bg = cocos.sprite.Sprite("res/field.png", position=(0,0), rotation=0, scale=1, opacity=255, color=(255, 255, 255), anchor=None)
+        bg = cocos.sprite.Sprite("res/field.png", position=(200,400))
         self.add(bg)
